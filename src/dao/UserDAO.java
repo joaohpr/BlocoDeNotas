260e387a98b7ceeb532c365ab8c55e6024fbd4e2
João <joaolucassilvasimoes123@gmail.com>
2025-12-24 17:38:15 -0300

    Implementando as funções da classe UserDAO,pacote DAO

--- src/dao/UserDAO.java
+++ src/dao/UserDAO.java
@@ -1,4 +1,50 @@
 package dao;
 
+import bd.BancoDeDadosUser;
+import model.User;
+
 public class UserDAO {
+
+    public UserDAO(){
+        this.usuarios = BancoDeDadosUser.getInstancia();
+    }
+
+   private final BancoDeDadosUser usuarios;
+
+
+    public void criarUser(User user){
+        usuarios.addUser(user);
+    }
+
+
+    public void removerUsuario(User userInput){
+        for(int i = 0;i < usuarios.size();i++){
+
+            User userComparar = usuarios.getUser(i);
+
+            if(userComparar.equals( userInput)){
+                usuarios.removeUser(i);
+                break;
+            }
+        }
+    }
+
+
+    public void mudarNomeUser(User user,String nomeSet){
+        user.setUserName(nomeSet);
+    }
+
+
+    public void mudarEmailUser(User user,String emailSet){
+        user.setEmailUser(emailSet);
+    }
+
+
+    public void mudarSenhaUser(User user,int senhaSet){
+        user.setSenhaUser(senhaSet);
+    }
+
+
+
+
 }
