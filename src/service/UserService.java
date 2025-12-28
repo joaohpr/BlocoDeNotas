06dfaa0de6014c007030a1f6f33b44d5ed5e6456
João <joaolucassilvasimoes123@gmail.com>
2025-12-28 17:47:14 -0300

    Criando e inplementando  a classe UserUtil

--- src/service/UserService.java
+++ src/service/UserService.java
@@ -2,61 +2,17 @@ package service;
 
 import dao.UserDAO;
 import model.User;
-
-
+import util.UserUtil;
 
 public class UserService {
 
-    User user = new User();
-    UserDAO userDao = new UserDAO();
-
-
-    public boolean userIsValid(User userInput) {
-
-        boolean userIsNull = false;
-        boolean userNameIsPresent = false;
-        boolean emailIsNull = false;
-        boolean senhaIsValid = false;
-
-        if (userInput == null) {
-            userIsNull = true;
-        } else {
-
-            if (userInput.getUserName() != null) {
-                userNameIsPresent = true;
-            }
-
-            if (userNameIsPresent) {
-                if (userInput.getEmailUser() == null) {
-                    emailIsNull = true;
-                }
-            }
-
-            if (userNameIsPresent) {
-                if (userInput.getSenhaUser() != 0) {
-                    senhaIsValid = true;
-                }
-            }
-        }
-
-        if (userIsNull) {
-            return true;
-        }
-
-        if (userNameIsPresent && (emailIsNull || senhaIsValid)) {
-            return true;
-        }
-
-        return false;
-
-    }
-
+    private final UserDAO userDao = new UserDAO();
 
     public String criarUser(String userName, String emailUser, int senhaUser) {
 
         User userAux = new User(userName, emailUser, senhaUser);
 
-        boolean isValid = userIsValid(userAux);
+        boolean isValid = UserUtil.userIsValid(userAux);
 
         if (isValid) {
             userDao.criarUser(userAux);
@@ -66,36 +22,37 @@ public class UserService {
         return "Alguma informação é nula ,TENTE NOVAMENTE! ";
     }
 
+    public String removerUser(String userName, String emailUser, int senha) {
 
-    public String removerUser(String userName,String emailUser,int senha) {
+        User userInput = new User(userName, emailUser, senha);
 
-        User userInput = new User(userName,emailUser,senha);
-
-        boolean isValid = userIsValid(userInput);
+        boolean isValid = UserUtil.userIsValid(userInput);
         boolean existe = userDao.userExite(userInput);
 
         if (isValid && existe) {
-
             userDao.removerUsuario(userInput);
             return " Usuario removido !";
         }
+
         return "Alguma coisa esta errada,TENTE NOVAMENTE! ";
     }
 
+    public String mudarNome(String userName, String emailUser, int senha) {
 
-    public String mudarNome(String userName,String emailUser,int senha){
+        User userAux = new User(userName, emailUser, senha);
 
-        User userAux = new User(userName,emailUser,senha);
-
-        if(userIsValid(userAux) && userDao.userExite(userAux)){
-            userDao.mudarNomeUser(userAux,userAux.getUserName());
+        if (UserUtil.userIsValid(userAux) && userDao.userExite(userAux)) {
+            userDao.mudarNomeUser(userAux, userAux.getUserName());
             return " Seu nome foi alterado!";
         }
+
         return "O nome não pode ser alterado!";
     }
 
 
+
+
+
+
+
 }
-
-
-
