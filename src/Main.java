8f2344a565c4fc27e9042740a13559a20bcc5933
João <joaolucassilvasimoes123@gmail.com>
2026-01-08 00:45:00 -0300

    Terminando a Opção 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import service.UserService;
-
-void main() {
+import java.util.Scanner;
+public class Main{
+public static void main(String [] args) throws InterruptedException {
 
     UserService userService = new UserService();
 
@@ -13,7 +14,7 @@ void main() {
             "2 - Remover usuário\n" +
             "3 - Alterar nome do usuário\n" +
             "4 - Alterar email do usuário\n" +
-            "0 - Sair\n" +
+            "5 - Sair\n" +
             "========================";
     String menuNotes = "===== MENU NOTAS =====\n" +
             "1 - Criar nota\n" +
@@ -21,12 +22,11 @@ void main() {
             "3 - Alterar texto da nota\n" +
             "4 - Remover todas as notas do usuário\n" +
             "5 - Listar todas as notas do usuário\n" +
-            "0 - Voltar / Sair\n" +
+            "6 - Voltar / Sair\n" +
             "======================";
 
 
-
-    do{
+    do {
         System.out.println(menuMain);
         optionMain = sc.nextInt();
 
@@ -36,85 +36,115 @@ void main() {
                 System.out.println(menuUser);
                 int optionUser = sc.nextInt();
 
-                switch(optionUser){
-                    case 1 ->{
-                        System.out.println("==== CRIANDO USUARIO ====");
+                if (optionUser <= 5 && optionUser >= 1) {
 
-                    System.out.print("Entre com seu nome : ");
-                    String nome = sc.next();
+                    do {
+                        switch (optionUser) {
+                            case 1 -> {
+                                System.out.println("==== CRIANDO USUARIO ====");
 
-                    System.out.print("\nEntre com seu email : ");
-                    String email = sc.next();
+                                System.out.print("Entre com seu nome : ");
+                                String nome = sc.next();
 
-                    System.out.print("\nEntre com uma senha de 6 digitos : ");
-                    int senha = sc.nextInt();
+                                System.out.print("\nEntre com seu email : ");
+                                String email = sc.next();
 
-                    userService.criarUser(nome,email,senha);
+                                System.out.print("\nEntre com uma senha de 6 digitos : ");
+                                int senha = sc.nextInt();
 
+                                userService.criarUser(nome, email, senha);
+
+                            }
+
+                            case 2 -> {
+                                System.out.println("==== REMOVENDO USUARIO ====");
+
+                                System.out.print("Informe seu nome : ");
+                                String nomeRemove = sc.next();
+
+                                System.out.print("\nEntre com seu email : ");
+                                String emailRemove = sc.next();
+
+                                System.out.print("\nEntre com sua senha : ");
+                                int senhaRemove = sc.nextInt();
+
+                                userService.removerUser(nomeRemove, senhaRemove);
+
+                            }
+
+                            case 3 -> {
+                                System.out.println("==== ALTERAR NOME ====");
+
+                                System.out.print("Entre com seu nome atual : ");
+                                String nomeNow = sc.next();
+
+                                System.out.print("\nEntre com o nome que vai substituir o atual : ");
+                                String nomeSet = sc.next();
+
+                                System.out.print("\n Entre com seu email : ");
+                                String email = sc.next();
+
+                                System.out.print("\nConfirme sua senha para alterar : ");
+                                int senha = sc.nextInt();
+
+                                userService.mudarNome(nomeNow, nomeSet, senha);
+
+                            }
+
+                            case 4 -> {
+                                System.out.println("==== ALTERAR EMAIL ====");
+
+                                System.out.print("Entre com seu nome :");
+                                String nome = sc.next();
+
+                                System.out.print("\n Confirme sua senha :");
+                                int senha = sc.nextInt();
+
+                                System.out.print("\nEntre com o email que ira substituir o atual :");
+                                String emailSet = sc.next();
+
+                                userService.mudarEmail(nome, senha, emailSet);
+
+                            }
+
+                        }
+
+                    } while (optionUser == 5);
+                    {
+                        System.out.println("Opcao invalida!");
                     }
+                } else {
+                    System.out.println("Opcao invalida!");
+                    break;
 
-                    case 2 ->{
-                        System.out.println("==== REMOVENDO USUARIO ====");
-
-                        System.out.print("Informe seu nome : ");
-                        String nomeRemove = sc.next();
-
-                        System.out.print("\nEntre com seu email : ");
-                        String emailRemove = sc.next();
-
-                        System.out.print("\nEntre com sua senha : ");
-                        int senhaRemove = sc.nextInt();
-
-                        userService.removerUser(nomeRemove,senhaRemove);
-
-                    }
-
-                    case 3 ->{
-                        System.out.println("==== ALTERAR NOME ====");
-
-                        System.out.print("Entre com seu nome atual : ");
-                        String nomeNow = sc.next();
-
-                        System.out.print("\nEntre com o nome que vai substituir o atual : ");
-                        String nomeSet = sc.next();
-
-                        System.out.print("\n Entre com seu email : ");
-                        String email = sc.next();
-
-                        System.out.print("\nConfirme sua senha para alterar : ");
-                        int senha = sc.nextInt();
-
-                        userService.mudarNome(nomeNow, nomeSet,senha);
-
-                    }
-
-                    case 4 ->{
-                        System.out.println("==== ALTERAR EMAIL ====");
-
-
-
-                    }
                 }
 
+            }
+
+            case 2 ->{
 
             }
 
-            case 2 -> {
-                System.out.println(menuNotes);
-                int optionNotes = sc.nextInt();
+            case 3 ->{
+                System.out.print("Saindo");
+                String point  = ".";
+                for(int i = 0;i < 3;i++){
+                    System.out.print(point);
+                    Thread.sleep(1000);
+
+                }
+                System.out.println("");
 
             }
 
-            case 3 -> {
+
+        }
+    } while (optionMain == 3) ;
+
+
+
+
+
 
             }
         }
-
-
-
-
-        }while(optionMain == 3);
-
-
-
-}
